407d3101c90940592b94c1f3f3a5cd1714ff23a6
LUSIFER <70184956+shado-hackers@users.noreply.github.com>
2022-06-15 17:22:00 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -7,8 +7,8 @@ You can upload HTTP/HTTPS direct link, Using this bot!
     FORMAT_SELECTION = "Select the desired format: <a href='{}'>file size might be approximate</a> \nIf you want to set custom thumbnail, send photo before or quickly after tapping on any of the below buttons.\nYou can use /deletethumbnail to delete the auto-generated thumbnail."
     SET_CUSTOM_USERNAME_PASSWORD = """If you want to download premium videos, provide in the following format:
 URL | filename | username | password"""
-    DOWNLOAD_START = "⇩⬇📩Now Downloading..⇊⇊"
-    UPLOAD_START = "⇑⇑Now Uploading..⇮⇮"
+    DOWNLOAD_START = "⤵️⬇️Now Downloading..⇊⇊"
+    UPLOAD_START = "⤴️⬆️Now Uploading..⇮⇮"
     RCHD_TG_API_LIMIT = "➠Downloaded in {} seconds.\nDetected File Size: {}\nSorry. But, I cannot upload files greater than 2GB due to Telegram API limitations."
     AFTER_SUCCESSFUL_UPLOAD_MSG = "➠Thanks for using @URLX_bot)"
     AFTER_SUCCESSFUL_UPLOAD_MSG_WITH_TS = "➥Downloaded in {} seconds.\nUploaded in {} seconds.\n\n@HEXAX_bot"
